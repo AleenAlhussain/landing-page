06b916ef8b95df5a46c687a5ee10de69c151cb11
AleenAlhussain <devaleenalhussain@gmail.com>
2024-11-25 14:37:59 +0300

    make landing page

---
+++ js/script.js
@@ -0,0 +1,44 @@
+(function () {
+  "use strict";
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  var forms = document.querySelectorAll(".needs-validation");
+
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms).forEach(function (form) {
+    form.addEventListener(
+      "submit",
+      function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+
+        form.classList.add("was-validated");
+      },
+      false
+    );
+  });
+})();
+
+let items2 = document.querySelectorAll(
+  "#featureContainer .carousel .carousel-item"
+);
+
+items2.forEach((el) => {
+  const minPerSlide = 4;
+  let next = el.nextElementSibling;
+  for (var i = 1; i < minPerSlide; i++) {
+    if (!next) {
+      // wrap carousel by using first child
+      next = items2[0];
+    }
+    let cloneChild = next.cloneNode(true);
+    el.appendChild(cloneChild.children[0]);
+    next = next.nextElementSibling;
+  }
+});
+$(document).ready(function () {
+  $("#featureCarousel").carousel({ interval: false });
+  $("#featureCarousel").carousel("pause");
+});
